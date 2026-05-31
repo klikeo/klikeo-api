774d475e047eb88a3faad2fc8edc9c5f77e11004
Didier Pereira <krrattoss5@gmail.com>
2026-05-31 14:16:38 -0500

    delete/dist

--- dist/routes/webhhoks.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const webhooks_controller_1 = require("../controllers/webhooks.controller");
-const route = (0, express_1.Router)();
-route.get("/whatsapp", webhooks_controller_1.whatsappVerificationController);
-route.post("/whatsapp", webhooks_controller_1.incommingMessagesFromMetaController);
-exports.default = route;
